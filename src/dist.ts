c738b6e274b9c9d9385f94eaa5782930e607491e
Padreramnt <padreramnt@gmail.ru>
2020-06-26 01:21:58 +1000

    fix dist global variables collision

--- src/dist.ts
+++ src/dist.ts
@@ -1,3 +1,5 @@
 import UIDateElement from './index'
 
 window.customElements.define('ui-date', UIDateElement)
+// Reflect.defineProperty(window, UIDateElement.name, { value: UIDateElement })
+export default UIDateElement
